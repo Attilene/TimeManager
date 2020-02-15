ef99c199fa6df996ad31d14e987232ac20fc1df1
T1MON <derbindima5@gmail.com>
2020-02-15 11:24:07 +0300

    /

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 class login_user:
-    # import schedule_access
+    import schedule_access as sa
     def __init__(self, log):
         self.log = log
-        self.week = week
+        self.week = sa.
