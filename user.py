380df51abceb481a2fdaf5b23bb652a537641f2b
T1MON <derbindima5@gmail.com>
2020-02-15 11:03:06 +0300

    Только начал

--- user.py
+++ user.py
@@ -2,7 +2,4 @@ class login_user:
     # import schedule_access
     def __init__(self, log):
         self.log = log
-
-    def week(self):
-        return
-    def day(self):
+        self.week = week
