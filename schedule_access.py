f029f3de0fc54058faf8590064c4c11d8c819a0b
attilene <artembakanov123@yandex.ru>
2020-02-18 21:21:36 +0300

    9 из 11 функций завершено

--- schedule_access.py
+++ schedule_access.py
@@ -39,12 +39,15 @@ def ret_day(log):
 	sel = f"SELECT * FROM day_{log}"
 	cur.execute(sel)
 	vyvod = cur.fetchall()
-	return tuple(sorted(vyvod, key = lambda x:x[0] * 60 + x[1]))
+	return tuple(sorted(vyvod, key = lambda x: x[0] * 60 + x[1]))
 
 def ret_month(log):
+	d = {'Январь':(0,31,), 'Февраль':(1,28,), 'Март':(2,31,), 'Апрель':(3,30,), 'Май':(4,31,), 'Июнь':(5,30,),
+		 'Июль':(6,31,), 'Август':(7,31,), 'Сентябрь':(8,30,), 'Октябрь':(9,31,), 'Ноябрь':(10,30,), 'Декабрь':(11,31,)}
 	sel = f"SELECT * FROM month_{log}"
 	cur.execute(sel)
-	return cur.fetchall()
+	vyvod = cur.fetchall()
+	return tuple(sorted(vyvod, key = lambda x: x[0] + d[x[1]][0] * d[x[1]][1]))
 
 def add_day(log, hours, mins, task):
 	ins = f"INSERT INTO day_{log} (hours, mins, task) VALUES (?, ?, ?)"
@@ -53,7 +56,7 @@ def add_day(log, hours, mins, task):
 
 def add_month(log, digit, month, task):
 	ins = f"INSERT INTO month_{log} (digit, month, task) VALUES (?, ?, ?)"
-	cur.execute(ins, [(digit), (month), (task)])
+	cur.execute(ins, [(digit), (month.capitalize()), (task)])
 	conn.commit()
 
 #def del_day(log, <тут пока хз>):
@@ -61,15 +64,20 @@ def add_month(log, digit, month, task):
 #def del_month(log, <тут пока хз>):
 
 
-log = 'kekno'
-psw = 'real'
-add_user(log, psw)
-add_user('kring','chebureck')
-cur.execute("SELECT * FROM users")
-print(cur.fetchall())
-add_day(log, 15, 30, 'just do it')
-add_day(log, 5, 30, 'lolkekchebureck')
-add_day(log, 18, 45, 'uuuuuuuu')
-add_day(log, 5, 40, 'sdfvgv')
-add_day(log, 4, 59, 'adsfdfsd')
-print(ret_day(log))
+# log = 'kekno'
+# psw = 'real'
+# add_user(log, psw)
+# add_user('kring','chebureck')
+# cur.execute("SELECT * FROM users")
+# print(cur.fetchall())
+# add_day(log, 15, 30, 'just do it')
+# add_day(log, 5, 30, 'lolkekchebureck')
+# add_day(log, 18, 45, 'uuuuuuuu')
+# add_day(log, 5, 40, 'sdfvgv')
+# add_day(log, 4, 59, 'adsfdfsd')
+# add_month(log, 13, 'февраль', 'sdfdgfs')
+# add_month(log, 1, 'январь', 'sdfывавпрdgfs')
+# add_month(log, 30, 'Сентябрь', 'sdfdgfsфцуыквгапть')
+# add_month(log, 23, 'июнь', 'sdФВАЫВППfdgfs')
+# print(ret_month(log))
+# print(ret_day(log))
