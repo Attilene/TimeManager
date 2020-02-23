c391bdc5e450a34a75836fcb64c6e34be0a0ce92
T1MON <derbindima5@gmail.com>
2020-02-23 09:12:02 +0300

    Изменил структуру проекта

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,5 @@
-import schedule_access
+from schedule_access import *
+from routes import *
 
+__all__ = ['add_user', 'del_user', 'change_pass', 'check_all', 'check_log', 'ret_day', 'ret_month', 'add_day',
+           'add_month']
