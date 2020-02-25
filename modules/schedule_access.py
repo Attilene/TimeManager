269820dbfc4273f7ffd96fcabe905ba7bef31c8e
T1MON <derbindima5@gmail.com>
2020-02-25 20:33:25 +0300

    Добавил поддержку списков

--- modules/schedule_access.py
+++ modules/schedule_access.py
@@ -6,25 +6,24 @@ class User(object):
 					(login VARCHAR(300), psw VARCHAR(300))""")
 	__month_list = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь', ]
 
-	def __init__(self, log, psw = None):
+	def __init__(self, log, psw=None):
 		self.log = log
 		if not User.check_log(self):
 			User.__add_user(self, psw)
-		self.list = User.__ret_list(self)
+		self.lists = User.__ret_list(self)
 		self.day = User.__ret_day(self)
 		self.month = User.__ret_month(self)
-		self.list = User.__ret_list(self)
 
 	def __ret_list(self):
-		dic = dict()
-		User.__cur.execute(f"SELECT (name) FROM list_{self.log}")
-		name = list(set(User.__cur.fetchall()))
-		print(name)
-		for i in range(len(name)):
-			User.__cur.execute(f"SELECT (task) FROM list_{self.log} WHERE name = ?", [(name[i])])
-			task  = tuple(User.__cur.fetchall())
-			dic.update({i:task})
-		return dict()
+		User.__cur.execute(f"SELECT * FROM list_{self.log}")
+		lists = {}
+		try:
+			for name, task in User.__cur.fetchall():
+				if task == 'empty' or name not in lists: lists[name] = []
+				else: lists[name].append(task)
+		except TypeError: return {}
+		for name in lists: lists[name] = sorted(lists[name])
+		return lists
 
 	def __ret_day(self):
 		User.__cur.execute(f"SELECT * FROM day_{self.log}")
@@ -36,38 +35,52 @@ class User(object):
 
 	def __add_user(self, psw):
 		ins = "INSERT INTO users (login, psw) VALUES (?, ?)"
-		User.__cur.execute(ins, [(self.log), (psw)])
+		User.__cur.execute(ins, [self.log, psw])
 		User.__conn.commit()
 		User.__cur.execute(f"CREATE TABLE IF NOT EXISTS month_{self.log} (digit INTEGER, month VARCHAR(50), task VARCHAR(1000))")
-		User.__cur.execute(f"CREATE TABLE IF NOT EXISTS day_{self.log} (hours INTEGER, mins INTEGER, task VARCHAR(1000))")
+		User.__cur.execute(f"CREATE TABLE IF NOT EXISTS day_{self.log} (hour INTEGER, minute INTEGER, task VARCHAR(1000))")
 		User.__cur.execute(f"CREATE TABLE IF NOT EXISTS list_{self.log} (name INTEGER, task VARCHAR(1000))")
 
 	def check_log(self):
 		sel = "SELECT (login) FROM users WHERE login=?"
-		User.__cur.execute(sel, [(self.log)])
+		User.__cur.execute(sel, [self.log])
 		return User.__cur.fetchone() is not None
+
+	def check_all(self, psw):
+		sel = "SELECT (psw) FROM users WHERE login=?"
+		User.__cur.execute(sel, [self.log])
+		return User.__cur.fetchone() == (psw,)
 
 	def change_pass(self, psw):
 		sel = "UPDATE users SET psw=? WHERE login=?"
-		User.__cur.execute(sel, [(psw), (self.log)])
+		User.__cur.execute(sel, [psw, self.log])
 		User.__conn.commit()
 
 	def del_user(self):
 		clr = "DELETE FROM users WHERE login=?"
-		User.__cur.execute(clr, [(self.log)])
+		User.__cur.execute(clr, [self.log])
 		User.__conn.commit()
 		User.__cur.execute(f"DROP TABLE IF EXISTS month_{self.log}")
 		User.__cur.execute(f"DROP TABLE IF EXISTS day_{self.log}")
 
-	def check_all(self, psw):
-		sel = "SELECT (psw) FROM users WHERE login=?"
-		User.__cur.execute(sel, [(self.log)])
-		return User.__cur.fetchone() == (psw,)
+	def add_list(self, name):
+		if name not in self.lists:
+			ins = f"INSERT INTO list_{self.log} (name, task) VALUES (?, ?)"
+			User.__cur.execute(ins, [name, 'empty'])
+			User.__conn.commit()
+			self.lists.update({name: []})
 
-	def add_day(self, hours, mins, task):
-		if (hours, mins, task) not in self.day:
-			ins = f"INSERT INTO day_{self.log} (hours, mins, task) VALUES (?, ?, ?)"
-			User.__cur.execute(ins, [(hours), (mins), (task)])
+	def add_list_task(self, name, task):
+		if task not in self.lists[name]:
+			ins = f"INSERT INTO list_{self.log} (name, task) VALUES (?, ?)"
+			User.__cur.execute(ins, [name, task])
+			User.__conn.commit()
+			self.lists = User.__ret_list(self)
+
+	def add_day(self, hour, minute, task):
+		if (hour, minute, task) not in self.day:
+			ins = f"INSERT INTO day_{self.log} (hour, minute, task) VALUES (?, ?, ?)"
+			User.__cur.execute(ins, [hour, minute, task])
 			User.__conn.commit()
 			self.day = User.__ret_day(self)
 
@@ -75,47 +88,75 @@ class User(object):
 		month = month.lower()
 		if (digit, month, task) not in self.month:
 			ins = f"INSERT INTO month_{self.log} (digit, month, task) VALUES (?, ?, ?)"
-			User.__cur.execute(ins, [(digit), (month), (task)])
+			User.__cur.execute(ins, [digit, month, task])
 			User.__conn.commit()
 			self.month = User.__ret_month(self)
 
-	def add_list(self, name, task):
-		if (name, task) not in self.list:
-			ins = f"INSERT INTO list_{self.log} (name, task) VALUES (?, ?)"
-			User.__cur.execute(ins, [(name), (task)])
+	def del_list(self, name):
+		if (name) in self.lists:
+			clr = f"DELETE FROM list_{self.log} WHERE name = ?"
+			User.__cur.execute(clr, name)
 			User.__conn.commit()
-			self.list = User.__ret_list(self)
+			self.lists.remove(name)
 
-	def del_day(self, hours, mins, task):
-		if (hours, mins, task) in self.day:
-			clr = f"DELETE FROM day_{self.log} WHERE hours = ? AND mins = ? AND task = ?"
-			User.__cur.execute(clr, [(hours), (mins), (task)])
+	def del_list_task(self, name, task):
+		if task in self.lists[name] and task != 0:
+			clr = f"DELETE FROM list_{self.log} WHERE name = ? AND task = ?"
+			User.__cur.execute(clr, [name, task])
 			User.__conn.commit()
-			self.day.remove((hours, mins, task))
+			self.lists[name].remove(task)
+
+	def del_day(self, hour, minute, task):
+		if (hour, minute, task) in self.day:
+			clr = f"DELETE FROM day_{self.log} WHERE hour = ? AND minute = ? AND task = ?"
+			User.__cur.execute(clr, [hour, minute, task])
+			User.__conn.commit()
+			self.day.remove((hour, minute, task))
 
 	def del_month(self, digit, month, task):
 		month = month.lower()
 		if (digit, month, task) in self.month:
 			clr = f"DELETE FROM month_{self.log} WHERE digit = ? AND month = ? AND task = ?"
-			User.__cur.execute(clr, [(digit), (month), (task)])
+			User.__cur.execute(clr, [digit, month, task])
 			User.__conn.commit()
 			self.month.remove((digit, month, task))
 
+	@staticmethod
+	def _erase():
+		"""Стирание всех пользователей"""
+		User.__cur.execute(f"SELECT login FROM users")
+		log_list = User.__cur.fetchall()
+		if len(log_list) > 0:
+			for log in log_list:
+				User.__cur.execute(f"DROP TABLE IF EXISTS list_{log[0]}")
+				User.__cur.execute(f"DROP TABLE IF EXISTS month_{log[0]}")
+				User.__cur.execute(f"DROP TABLE IF EXISTS day_{log[0]}")
+		User.__cur.execute(f"DELETE from users")
 
 # Мои тесты (Дима)
-now_user = User("T1MON", 'kdfjdkffj')
-now_user.add_month(23, 'январь', 'dfjfkdjf')
-now_user.add_month(24, 'январь', 'dfjfkdjf')
-now_user.add_month(25, 'январь', 'dfjfkdjf')
-now_user.add_list(1, 'dfjfkdjfdsfdgf')
-now_user.add_list(2, 'dfjf')
-now_user.add_list(1, 'dfjfkdjfdsfdgfasdfgdhfgjhjrsdv')
-now_user.add_day(22, 33, 'jdfkjf')
-now_user.add_day(24, 33, 'jdfkjf')
-now_user.add_day(23, 33, 'jdfkjf')
-print(now_user.day)
-print(now_user.month)
-print(now_user.list)
+# User._erase()
+# now_user = User("T1MON", 'kdfjdkffj')
+# print(now_user.day)
+# print(now_user.month)
+# print(now_user.lists)
+#
+# now_user.add_month(23, 'январь', 'dfjfkdjf')
+# now_user.add_month(24, 'январь', 'dfjfkdjf')
+# now_user.add_month(25, 'январь', 'dfjfkdjf')
+# now_user.add_list(1)
+# now_user.add_list(2)
+# now_user.add_list_task(1, 'dfjfkdjfdsfdgf')
+# now_user.add_list_task(2, 'dfjf')
+# now_user.add_list_task(1, 'dfjfkdjfdsfdgfasdfgdhfgjhjrsdv')
+# now_user.add_day(22, 33, 'jdfkjf')
+# now_user.add_day(24, 33, 'jdfkjf')
+# now_user.add_day(23, 33, 'jdfkjf')
+# now_user.del_day(23, 33, 'jdfkjf')
+# print(now_user.day)
+# print(now_user.month)
+# print(now_user.lists)
+
+# User._erase()
 
 # self.log = 'kekno'
 # psw = 'real'
