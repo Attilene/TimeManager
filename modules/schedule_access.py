069e33d1200e2323817c1a27784d8288c156074b
T1MON <derbindima5@gmail.com>
2020-03-16 21:49:16 +0300

    Релизнул данную стадию

--- modules/schedule_access.py
+++ modules/schedule_access.py
@@ -3,9 +3,9 @@ class User(object):
     from file_routes import db
     from werkzeug.security import generate_password_hash, check_password_hash
     if __name__ == "__main__":
-        __conn = sqlite3.connect(f"../{db}/schedule.db")
+        __conn = sqlite3.connect(f"../{db}/schedule.db", check_same_thread= False)
     else:
-        __conn = sqlite3.connect(f"{db}/schedule.db")
+        __conn = sqlite3.connect(f"{db}/schedule.db", check_same_thread= False)
     __gen = generate_password_hash
     __check = check_password_hash
     __cur = __conn.cursor()
