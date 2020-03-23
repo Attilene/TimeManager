d50d04ce5d9ad778d062deb2120b7686feabf792
T1MON <derbindima5@gmail.com>
2020-03-23 11:29:13 +0400

    переделал анимации

--- scriptes/script.js
+++ scriptes/script.js
@@ -1,4 +1,39 @@
-function changeTheme(color) {
-    $("aside .theme").addClass("hidden")
-    $("aside .theme button::after").addClass(color)
-}
+jQuery(document).ready(function () {
+    // Кнопки изменения цвета
+    $('aside .light button.red').on('click', function () {
+        $('body').attr('id', 'red')
+    });
+    $('aside .light button.blue').on('click', function () {
+        $('body').attr('id', 'blue')
+    });
+    $('aside .light button.green').on('click', function () {
+        $('body').attr('id', 'green')
+    });
+    $('aside .light button.purple').on('click', function () {
+        $('body').attr('id', 'purple')
+    });
+    $('aside .light button.sky').on('click', function () {
+        $('body').attr('id', 'sky')
+    });
+    $('aside .light button.black').on('click', function () {
+        $('body').attr('id', 'black')
+    });
+    $('aside .dark button.red').on('click', function () {
+        $('body').attr('id', 'red')
+    });
+    $('aside .dark button.blue').on('click', function () {
+        $('body').attr('id', 'blue')
+    });
+    $('aside .dark button.green').on('click', function () {
+        $('body').attr('id', 'green')
+    });
+    $('aside .dark button.purple').on('click', function () {
+        $('body').attr('id', 'purple')
+    });
+    $('aside .dark button.sky').on('click', function () {
+        $('body').attr('id', 'sky')
+    });
+    $('aside .dark button.black').on('click', function () {
+        $('body').attr('id', 'black')
+    });
+});
