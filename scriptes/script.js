d335d95f2e0d202d1c69dc5727784c162da1c043
T1MON <derbindima5@gmail.com>
2020-03-24 23:07:49 +0400

    Добавил динамический favicon Связал тему с БД

--- scriptes/script.js
+++ scriptes/script.js
@@ -11,20 +11,39 @@ function send(url, request) {
 
 jQuery(document).ready(function () {
 
+    // Получение данных от сервера
+    const roots = $('#roots');
+    const css = roots.data('css');
+    const img = roots.data('img');
+    const scr = roots.data('scr');
+    var theme = roots.data('theme');
+    var color = roots.data('color');
+
+    // Анимация окна
     $('header .right').on('click', function () {
-        $('body aside').fadeToggle(1000);
+        const aside = $('body aside');
+        if (aside.attr('class') === 'opened') {aside.removeClass('opened').fadeOut(200).animate({top: "60px"},
+        )}
+        else {aside.fadeIn(300).addClass('opened')}
     });
 
     // Кнопки изменения цвета
     $('aside .theme button').on('click', function () {
-        const theme = $('aside menu .theme').attr('class').split(' ')[1];
-        const color = $(this).attr('class');
-        $('html').attr('id', theme);
-        $('body').attr('id', color);
-        const favicon = $('head link[type="image/x-icon"]');
-        const favicon_href = favicon.attr('href').slice(0, favicon.attr('href').lastIndexOf('favicons') + 8);
-        favicon.attr('href', `${favicon_href}/${color}.svg`);
-        send('/change_theme', `${theme} ${color}`);
-    });
+        const new_theme = $(this).attr('class').split(' ')[0];
+        const new_color = $(this).attr('class').split(' ')[1];
+        if (new_theme !== theme) {
+            $('link#base_theme').attr('href', `${css}/base/${new_theme}.css`);
+            $('link#profile_theme').attr('href', `${css}/profile/${new_theme}.css`);
+        }
+        if (new_color !== color) {
+            $('link#favicon_choice').attr('href', `${img}/favicons/${new_color}.svg`);
+            $('link#color_choice').attr('href', `${css}/colors/${new_color}.css`);
+        }
+        if ((new_theme !== theme) || (new_color !== color)) {
+            send('/change_theme', `${new_theme} ${new_color}`);
+            theme = new_theme;
+            color = new_color;
+        }
+    })
 
 });
