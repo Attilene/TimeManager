db68a8af865e7bab2fdad2ca30aa11d13c9dea9b
T1MON <derbindima5@gmail.com>
2020-04-11 16:39:02 +0400

    .

--- modules/security.py
+++ modules/security.py
@@ -1,55 +1,10 @@
-# from random import choices
-# from string import printable
-# from werkzeug.security import check_password_hash, generate_password_hash, gen_salt
-# import rsa
-#
-# private_key = ''
-# public_key = ''
-#
-# def pakaging():
-#     global private_key, public_key
-#     public_key, private_key = newkeys(2048)
-#
-# import rsa
-#
-# (pubkey, privkey) = rsa.newkeys(512)
-#
-# message = b'Hello Blablacode.ru!'
-#
-# # шифруем
-# crypto = rsa.encrypt(message, pubkey)
-# print(crypto)
-# # расшифровываем
-# message = rsa.decrypt(crypto, privkey)
-# print(message)
-#
-#
-# (pubkey, privkey) = rsa.newkeys(512)
-# message = b'hello Alisa!'
-# crypto = rsa.encrypt(message, pubkey) # Зашифровка
-# print(pubkey)
-# print(crypto)
-# message = rsa.decrypt(crypto, privkey) # Расшифровка
-# print(message.decode('utf8'))
-#
-# privkey = rsa.PrivateKey(59861460478085564020717569863841455703046939406028997076971665720270739373047, 65537, 49342752284765250987429754819637445657484943506082288115895623420436999894553, 45901362813563279566235867408929868541277, 1304132531341689269119203120370247011)
-#
-#
-# import codecs
-#
-# hexify = codecs.getdecoder('hex')
-# crypto = hexify('175eb0732a93a6bc12f6bb4e3157d194e7ab64213b81021e4ac2d2576ba93b340369c973df0cbe2755490676069e4303da247c3b8fbfd09cc0d35234cc28aa047a25db80348e630cbbac3385a6')
-# print(crypto)
-# print(rsa.bytes2int(crypto))
-#
-#
-#
-#
-# print(crypto)
-#
-# message = rsa.decrypt(crypto, privkey)  # Расшифровка
-# print(message)
-#
-#
+from Crypto.PublicKey import RSA
+# 1024 means the keysize will be 1024 bits
+key_pair = RSA.generate(1024)
+private_key = open("privatekey.pem", "w")
+private_key.write(key_pair.exportKey().decode('utf-8'))
+private_key.close()
+public_key = open("public_key.pem", "w")
+public_key.write(key_pair.publickey().exportKey().decode('utf-8'))
+public_key.close()
 
-import PyCript
