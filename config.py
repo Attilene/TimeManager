62860686e4fbeab98c5e0d306698f90eee8e91e6
T1MON <derbindima5@gmail.com>
2020-05-17 21:21:46 +0400

    .

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ class Config(object):
     SEND_FILE_MAX_AGE_DEFAULT = 0  # TODO: Удалить в релизе
     CSRF_ENABLED = True
     SECRET_KEY = 'f92BjcBnNDIgd4CXe7sfMHpz7faEHyu0f07F6Ya2KG16c1xIoVFa3YdlkFm0lcj2vAkgxR2dIaRvR2kWR1CuOVyljL8MYhpF6tKU'
-    MAIL_SERVER = 'smtp.yandex.ru'
-    MAIL_PORT = 465
+    MAIL_SERVER = 'smtp.msndr.net'
+    MAIL_PORT = 25
     MAIL_DEBUG = False
     MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = 'Time-Manager.Team@yandex.ru'
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = 'derbindima5@gmail.com'
     MAIL_DEFAULT_SENDER = 'Time-Manager.Team@yandex.ru'
-    MAIL_PASSWORD = 'nhprtwrkaksngwxn'
+    MAIL_PASSWORD = '92254117ab450adf1fb028afb62e3d89'
 
 
