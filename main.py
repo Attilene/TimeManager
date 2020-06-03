622a72f13eee1c242d18ae00174e7715af0b255c
T1MON <derbindima5@gmail.com>
2020-06-03 10:14:55 +0400

    Исправил Гостя Версия 1.1

--- main.py
+++ main.py
@@ -4,3 +4,26 @@ create_keys()  # Обновление ключей
 
 if __name__ == "__main__":
     tm.run(host='127.0.0.1', port=5000)
+
+# <h1 style="text-align: center"> Best team </h1>
+#
+# <h2> Includes: </h2>
+#
+# <ul>
+#     <li>
+#                   (づ￣ ³￣)づ             | T1MON |
+#     </li><li>
+#                   ᕦ(ò_óˇ)ᕤ              | ATTILENE |
+#     </li><li>
+#                   ┌(ಠ_ಠ)┘               | MACMILLAN |
+#     </li><li>
+#                   (ﾉ◕ヮ◕)ﾉ           | PRAGMATIC FRIEND |
+#     </li><li>
+#                   (❍ᴥ❍ʋ)                | TRIIKSTER |
+#     </li>
+# </ul>
+#
+# <b>Time_Manager.com</b>
+#
+# Version 1.1
+# 03.06.2020
