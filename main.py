7e78004fd9f991e54f44b323b1b4197782182c80
T1MON <derbindima5@gmail.com>
2020-06-04 15:02:37 +0400

    Версия 1.4

--- main.py
+++ main.py
@@ -1,39 +1,6 @@
-"""
-Python 3.8
-Required Modules:
-1) Flask (full-packet)
-2) Flask-Mail
-3) Flask-Script
-4) Werkzeug
-5) Pycryptodome
-"""
-
 from modules import *
 
 create_keys()  # Обновление RSA ключей
 
 if __name__ == "__main__":
     tm.run(host='127.0.0.1', port=5000)
-
-# <h1 style="text-align: center"> Best team </h1>
-#
-# <h2> Includes: </h2>
-#
-# <ul>
-#     <li>
-#                   (づ￣ ³￣)づ             | T1MON |
-#     </li><li>
-#                   ᕦ(ò_óˇ)ᕤ              | ATTILENE |
-#     </li><li>
-#                   ┌(ಠ_ಠ)┘               | MACMILLAN |
-#     </li><li>
-#                   (ﾉ◕ヮ◕)ﾉ           | PRAGMATIC FRIEND |
-#     </li><li>
-#                   (❍ᴥ❍ʋ)                | TRIIKSTER |
-#     </li>
-# </ul>
-#
-# <b>Time_Manager.com</b>
-#
-# Version 1.4.0
-# 03.06.2020
